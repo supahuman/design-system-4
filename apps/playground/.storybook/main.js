8ba66481938dcf2fb1a102369cb788fdbeac7bf3
supahuman <successkoncepts@gmail.com>
2025-09-07 11:16:48 -0500

    feat: update Storybook configuration and move story files to the stories directory

---
+++ apps/playground/.storybook/main.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var config = {
+    stories: ['../stories/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+    addons: [],
+    framework: {
+        name: '@storybook/react-vite',
+        options: {
+            builder: {
+                viteConfigPath: 'vite.config.ts',
+            },
+        },
+    },
+};
+exports.default = config;
+// To customize your Vite configuration you can use the viteFinal field.
+// Check https://storybook.js.org/docs/react/builders/vite#configuration
+// and https://nx.dev/recipes/storybook/custom-builder-configs
