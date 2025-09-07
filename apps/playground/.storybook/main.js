6246d1eb28433c97bccf16533d9acb897536c1a3
supahuman <successkoncepts@gmail.com>
2025-09-07 11:18:53 -0500

    fix: standardize string quotes in Storybook configuration files

--- apps/playground/.storybook/main.js
+++ apps/playground/.storybook/main.js
@@ -1,16 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 var config = {
-    stories: ['../stories/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-    addons: [],
-    framework: {
-        name: '@storybook/react-vite',
-        options: {
-            builder: {
-                viteConfigPath: 'vite.config.ts',
-            },
-        },
+  stories: ['../stories/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  addons: [],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {
+      builder: {
+        viteConfigPath: 'vite.config.ts',
+      },
     },
+  },
 };
 exports.default = config;
 // To customize your Vite configuration you can use the viteFinal field.
